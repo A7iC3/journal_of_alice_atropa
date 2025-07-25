0b62114d2494c12a8445b4269103f229cc49f9a4
Sarah Alice <sagostinacchio@hotmail.com>
2025-07-25 07:39:34 -0400

    Added to gallery Alteration to tabs

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,33 @@
 const navTabs = document.querySelectorAll(".nav__btn");
 const contentGreeting = document.querySelector("#greeting");
-//const tabContent = document.querySelectorAll(".content__info");
+const galleryTemplate = document.querySelector("#gallery-template").content;
+const previewTemplate = document.querySelector("#preview-template").content;
+const galleryList = document.querySelector(".gallery__list");
+const galleryLog = [
+  {
+    name: "First Independent Project with TripleTen",
+    link: "https://a7ic3.github.io/first_independent_project/",
+    title: "First Independent Project",
+    preview: "./images/sarah-alice-first-independent-project.png",
+  },
+  {
+    name: "SE Project Library with TripleTen",
+    link: "https://a7ic3.github.io/se_project_library/",
+    title: "Software Engineering Project Library",
+    preview: "./images/sarah-alice-se-project-library.png",
+  },
+  {
+    name: "SE Project Coffeeshop with TripleTen",
+    link: "https://a7ic3.github.io/se_project_coffeeshop/",
+    title: "Software Engineering Project Coffeeshop",
+    preview: "./images/sarah-alice-se-project-coffeeshop.png",
+  },
+  {
+    name: "SE Project Spots with TripleTen",
+    link: "https://a7ic3.github.io/se_project_spots/",
+    title: "Software Engineering Project Spots",
+  },
+];
 
 function changeTab(arg1) {
   const currentTab = document.querySelector(".nav__btn.nav_active");
@@ -27,8 +54,48 @@ function changeTab(arg1) {
   newTabContent.classList.add("content_active");
 }
 
+function addToGallery(obj) {
+  //failsafe
+  if (!obj) return;
+  galleryList.prepend(obj);
+}
+
+function prepGalleryPiece(obj) {
+  //failsafe
+  if (!obj || typeof obj !== "object" || !obj.name || !obj.title || !obj.link) {
+    console.log(
+      "Missing Essential Data or invalid argument. Skipping element."
+    );
+    return null;
+  }
+  //main function
+  let newPiece;
+  //live preview
+  if (!obj.preview) {
+    newPiece = galleryTemplate.cloneNode(true);
+    const newPieceFrame = newPiece.querySelector(".gallery__iframe");
+    newPieceFrame.src = obj.link;
+    newPieceFrame.title = obj.title;
+  }
+  //img preview
+  else {
+    newPiece = previewTemplate.cloneNode(true);
+    const newPieceImg = newPiece.querySelector(".gallery__preview");
+    newPieceImg.src = obj.preview;
+    newPieceImg.alt = obj.title;
+  }
+  const newPieceAnchor = newPiece.querySelector(".gallery__link");
+  newPieceAnchor.href = obj.link;
+  newPieceAnchor.textContent = obj.name;
+  return newPiece;
+}
+
 navTabs.forEach((navTab) => {
   navTab.addEventListener("click", () => {
     changeTab(navTab);
   });
+});
+
+galleryLog.forEach((galleryItem) => {
+  addToGallery(prepGalleryPiece(galleryItem));
 });
